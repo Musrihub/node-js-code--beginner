071635712ad9efe5bd4bb624c0c0cad952202bb1
Lionel NTIRUSHWA <linog02@gmail.com>
2024-01-08 10:35:36 +0200

    write file

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ console.log(hello);
 const fs = require('fs');
 const text = fs.readFileSync('./text/input.txt', 'utf-8');
 console.log(text);
+
+const textout = `This is my greetings:${text}.\n Created ${Date.now()}`;
+fs.writeFileSync('./text/output.txt', textout);
+console.log("file written");
